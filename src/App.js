7f258c582df94e7c994b2c87a6c62c743639d70f
Earl Hans Genoso <earlhansg@gmail.com>
2023-05-25 03:41:26 +0800

    T1-The task state has been enhanced to include two objects: todoBoard and doneBoard. The drag and drop functionality has already been implemented and is functioning properly with the current state.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return ();
+  const [task, setTask] = useState({
+    todoBoard: ['Task 1', 'Task 2', 'Task 3'],
+    doneBoard: []
+  })
+  function handleOnDrag(e: React.DragEvent, cardType) {
+    console.log("handleOnDrag", cardType.id);
+    e.dataTransfer.setData("widgetType", cardType.taskName.concat('#' + cardType.id.toString()));
+  }
+
+  function handleOnDrop(e: React.DragEvent) {
+    const widgetType = e.dataTransfer.getData("widgetType");
+    const droppedTaskIndex = parseInt(widgetType.split('#')[1]);
+    const filteredTodo = task.todoBoard.filter((item, id) => id !== droppedTaskIndex);
+    console.log("filteredTodo", filteredTodo)
+    setTask((prev) => ({
+      ...prev,
+      todoBoard: [...filteredTodo],
+      doneBoard: [...prev.doneBoard, widgetType.split('#')[0]]}))
+  }
+
+  function handleDragOver(e: React.DragEvent) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <div className="todo">
+        {task.todoBoard.map((taskName, index)=>
+          (<div className="card" key={index} draggable onDragStart={(e) => handleOnDrag(e, {taskName, id: index})}>
+            {taskName}
+          </div>)
+        )}
+      </div>
+      <div className="done" onDrop={handleOnDrop} onDragOver={handleDragOver} style={task.doneBoard?.length > 2 ? {height: "auto"} : {height: "350px"} }>
+        {task.doneBoard.map((widget, index) => (
+          <div className="card" key={index}>
+            {widget}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
