a0ae6b9137b69d586991150c157611fa9abe42b9
Earl Hans Genoso <earlhansg@gmail.com>
2023-05-26 01:06:28 +0800

    T2-added styles in Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Todo({task, handleOnDrag}) {
   return (
-    <div className="todo">
+    <div className="todo" style={task.todoBoard?.length > 2 ? {height: "auto"} : {height: "350px"} }>
         {task.todoBoard.map((taskName, index)=>
           (<div className="card" key={index} draggable onDragStart={(e) => handleOnDrag(e, {taskName, id: index})}>
             {taskName}
