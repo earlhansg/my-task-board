0610015c5ed37273f68512b4c5aeed9e22bb91d1
Earl Hans Genoso <earlhansg@gmail.com>
2023-05-27 05:34:30 +0800

    T3-installed materialUi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import "./App.css";
 import Todo from "./components/Todo";
 import Done from "./components/Done";
